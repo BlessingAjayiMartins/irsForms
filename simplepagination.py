49fe07e2e1bc4017daca3706f5e1c275ff2e6ab6
Blessing Ajayi-Martins <blessmartins@gmail.com>
2021-11-08 11:07:44 -0500

    working/removed database check

--- simplepagination.py
+++ simplepagination.py
@@ -8,8 +8,7 @@ import json
 
 
 def main():
-  # check if database.txt exists
-  if not os.path.exists("database.txt"):
+  
     database = {}
     index_count = 0
     for x in range(1, 103):
@@ -101,140 +100,7 @@ def main():
             if str(x) in data[form_lookup]:
               pdf_link_list.append(data[form_lookup][str(x)]["link"])
               available_date_in_range.append(str(x))
-              # pdf_url = data[form_lookup][str(x)]["link"]
-              # response = requests.get(pdf_url)
-              # pdf = open()
-          
-          x=0
-          for pdf in pdf_link_list:
-            
-            date_as_key = available_date_in_range[x]
-            # if the directory does NOT exist
-            if not os.path.exists(form_lookup):
-              print("creating subdirectory..")
-
-              os.mkdir(form_lookup, mode = 0o666)
-
-              # request_download = requests.get(pdf, stream=True)
-              response = requests.get(pdf)
-              # request_download = BeautifulSoup(response.content, "html.parser")
-
-              # Navigate to the desired directory
-              os.chdir(f"{form_lookup}")
-              # Stream and save the data
-              with open(f"{form_lookup} - {date_as_key}.pdf", 'wb') as f:
-                  f.write(response.content)
               
-              # print(f"Downloading {date_as_key} \n")
-
-                # Go up a level
-              os.chdir('..')
-
-              print("Enjoy your downloads!")
-            # Else if the directory does exist
-            elif os.path.exists(form_lookup):
-                # Change to the desired directory
-                os.chdir(f'{form_lookup}')
-                # Get the data from the url
-                response = requests.get(pdf)
-                # Stream the data to be saved
-                with open(f"{form_lookup} - {date_as_key}.pdf", 'wb') as f:
-                  f.write(response.content)
-                print("Enjoy your download! \n")
-                # Go up a level, useful if cell is ran multiple times
-                os.chdir('..')
-            
-            x = x+1
-        elif len(range_input) == 4:
-          
-          if range_input in data[form_lookup]:
-            pdf_link = data[form_lookup][range_input]["link"]
-            # if directory does not exist
-            if not os.path.exists(form_lookup):
-              print("creating subdirectory..")
-
-              os.mkdir(form_lookup, mode = 0o666)
-
-            
-              response = requests.get(pdf_link)
-              
-
-              # Navigate to the desired directory
-              os.chdir(f"{form_lookup}")
-              # Save the data
-              with open(f"{form_lookup} - {range_input}.pdf", 'wb') as f:
-                  f.write(response.content)
-                # Go up a level
-              os.chdir('..')
-
-              print("Enjoy your downloads!")
-            # Else if the directory does exist
-            elif os.path.exists(form_lookup):
-                # Change to the desired directory
-                os.chdir(f'{form_lookup}')
-                # Get the data from the url
-                response = requests.get(pdf_link)
-                # Stream the data to be saved
-                with open(f"{form_lookup} - {range_input}.pdf", 'wb') as f:
-                  f.write(response.content)
-                print("Enjoy your download! ")
-                # Go up a level
-                os.chdir('..')
-
-          else:
-            print("Date not available")
-
-      else:
-        print("Form not available.")
-  else:
-    #First utility 
-
-    with open ("database.txt") as json_file:
-      data = json.load(json_file)
-      form_lookup = input("enter list of forms seperated by comma. ex: Form W-2,Form 1095-C : ")
-      form_list = form_lookup.split(',')
-      date_list = []
-      result = []
-      for form in form_list:
-        # print(form)
-        if data[form]:
-          date_range = data[form].keys()
-          
-          for date in date_range:
-            date_list.append(int(date))
-              
-          form_title = data[form][str(date_list[0])]["title"]
-          result.append({
-            "form_number": form,
-            "form_title": form_title,
-            "min_year": min(date_list),
-            "max_year": max(date_list)
-            })
-          date_list = []
-        else:
-          print("form not found")
-        print(result)
-
-    # Second utility
-
-    with open ("database.txt") as json_file:
-      data = json.load(json_file)
-
-      form_lookup = input("please type in form name : ")
-      if data[form_lookup]:
-        range_input = input("type in year xxxx or range seperated by - ex:2012-2014 : ")
-        if len(range_input) == 9:
-          range_array = range_input.split('-')
-          available_date_in_range = []
-          pdf_link_list = []
-          
-          for x in range (int(range_array[0]), int(range_array[1])):
-            if str(x) in data[form_lookup]:
-              pdf_link_list.append(data[form_lookup][str(x)]["link"])
-              available_date_in_range.append(str(x))
-              # pdf_url = data[form_lookup][str(x)]["link"]
-              # response = requests.get(pdf_url)
-              # pdf = open()
           
           x=0
           for pdf in pdf_link_list:
@@ -319,7 +185,5 @@ def main():
         print("Form not available.")
     
 main()
-# if __name__ == '__main__':
-#   import sys
-#   print('args are', sys.argv)
+
 
